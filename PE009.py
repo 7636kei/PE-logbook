d2f1147b3d75f754db842ff63011d61040ffa371
7636kei <75885226+7636kei@users.noreply.github.com>
2020-12-13 14:03:42 +0700

    Add files via upload

---
+++ PE009.py
@@ -0,0 +1,21 @@
+import math
+
+def tripletproduct(targ_sum):
+	#three reserved return variables
+	ret_a = 0
+	ret_b = 0
+	ret_c = 0
+	for i in range(0, math.ceil(targ_sum/3)):
+		for j in range (0, math.ceil(math.sqrt(targ_sum**2 - i**2))):
+			if i**2 + j**2 == (targ_sum - i - j)**2:
+				ret_a = i
+				ret_b = j
+				ret_c = (targ_sum - i - j)
+				break
+	if ret_a == 0: #nothing found
+		print("No triplet found :(")
+	else: #we found something
+		print("Triplet found:", ret_a, ret_b, ret_c)
+		print("Product=", ret_a * ret_b * ret_c)
+
+tripletproduct(1000)
